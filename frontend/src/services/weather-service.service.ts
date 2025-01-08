b07267b184f1f515fb232bf5d0181fc24632f9cc
iamhappypai <santhoshsomanathapai@gmail.com>
2025-01-09 00:17:17 +0530

    updated forecastmodel similar to BE

--- frontend/src/services/weather-service.service.ts
+++ frontend/src/services/weather-service.service.ts
@@ -2,15 +2,28 @@ import * as backendApi from '../shared/backend-api';
 
 export interface ForecastModel {
   latitude: number,
-  longitude: number,
-  elevation: number,
-  current_weather: {
-      temperature: number,
-      windspeed: number,
-      winddirection: number,
-      weathercode: number,
-      time: Date
-  }
+    longitude: number,
+    elevation: number,
+    generationtime_ms: number,
+    timezone: string,
+    timezone_abbreviation: string,
+    utc_offset_seconds: string,
+    current_weather: {
+        temperature: number,
+        windspeed: number,
+        winddirection: number,
+        weathercode: number,
+        time: Date
+    },
+    current_weather_units: {
+        time: string,
+        interval: string,
+        temperature: string,
+        windspeed: string,
+        winddirection: string,
+        is_day: string,
+        weathercode: string
+    }
 }
 export default class WeatherService {
   constructor() {}
