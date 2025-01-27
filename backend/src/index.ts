e3813df3bb5e4d7805b78cc7f3430709d61e6d45
iamhappypai <santhoshsomanathapai@gmail.com>
2025-01-27 23:04:15 +0530

    basic docker setup done. needs some more work to get it working

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const port = 3003;
 
 app.use(cors());
 app.use(express.json() as express.RequestHandler);
