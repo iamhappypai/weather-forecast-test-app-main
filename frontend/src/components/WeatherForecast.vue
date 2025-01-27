572fc416e65491705925fb767e8b6dc37ae0648b
iamhappypai <santhoshsomanathapai@gmail.com>
2025-01-27 23:35:34 +0530

    minor style correction done for error handling

--- frontend/src/components/WeatherForecast.vue
+++ frontend/src/components/WeatherForecast.vue
@@ -30,7 +30,7 @@
     </div>
 
     <div v-if="apiErrorMessage && !weatherForecast?.value" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-lg" role="alert">
-      <span class="block sm:inline">
+      <span class="block">
         {{ apiErrorMessage }}
       </span>
     </div>
